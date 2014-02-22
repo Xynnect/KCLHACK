af6215c519191ad2b4b06a5bb4a89d6dee81f233
Xynnect <dido.jordanov7@gmail.com>
2014-02-22 15:34:17 +0000

    resolved the issue with the settingBuilder

--- src/kcl/hackton/hackproject/Main.java
+++ src/kcl/hackton/hackproject/Main.java
@@ -1,22 +1,31 @@
 package kcl.hackton.hackproject;
 
+import com.novoda.imageloader.core.*;
+import com.novoda.imageloader.core.cache.LruBitmapCache;
+import com.novoda.imageloader.core.util.DirectLoader;
+import com.novoda.imageloader.core.LoaderSettings.SettingsBuilder;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.view.Menu;
 
 public class Main extends Activity {
+	
 
+	DirectLoader dl = new DirectLoader();
+	Bitmap b = dl.download("http://www.asianweek.com/wp-content/uploads/2012/03/microsoft_logo11.jpg");
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+		normalImageManagerSettings();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void normalImageManagerSettings() {
+		ImageManager imageManager = new ImageManager(this, new SettingsBuilder()
+	        .withCacheManager(new LruBitmapCache(this)).build(this));
 	}
+	
 
 }
